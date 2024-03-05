0bda66de27c26045e4660c4b7d71ae77b0b2b3cf
artNehay <artemynehay@gmail.com>
2024-03-05 14:14:24 +0300

    Add custom colors for the color schemes

---
+++ app/src/main/java/com/example/android/insightnews/ui/theme/Color.kt
@@ -0,0 +1,31 @@
+package com.example.android.insightnews.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_primary = Color(0xFFFF3C3C)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_secondary = Color(0xFF5A489B)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_background = Color(0xFFFFFFFF)
+val md_theme_light_onBackground = Color(0xFF252729)
+val md_theme_light_surface = Color(0xFFFFFFFF)
+val md_theme_light_onSurface = Color(0xFF252729)
+val md_theme_light_onSurfaceVariant = Color(0xFF5F646C)
+val md_theme_light_error = Color(0xFFFA4856)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_outline = Color(0xFFC6C6C8)
+val md_theme_light_scrim = Color(0xFFAEB2B9)
+
+val md_theme_dark_primary = Color(0xFFFF3C3C)
+val md_theme_dark_onPrimary = Color(0xFFFFFFFF)
+val md_theme_dark_secondary = Color(0xFF5A489B)
+val md_theme_dark_onSecondary = Color(0xFFFFFFFF)
+val md_theme_dark_background = Color(0xFF11121C)
+val md_theme_dark_onBackground = Color(0xFFFAFAFA)
+val md_theme_dark_surface = Color(0xFF00040C)
+val md_theme_dark_onSurface = Color(0xFFFAFAFA)
+val md_theme_dark_onSurfaceVariant = Color(0xFFBCBCBC)
+val md_theme_dark_error = Color(0xFFFA4856)
+val md_theme_dark_onError = Color(0xFFFFFFFF)
+val md_theme_dark_outline = Color(0xFFC6C6C8)
+val md_theme_dark_scrim = Color(0xFFAEB2B9)
--- app/src/main/java/com/example/android/insightnews/ui/theme/Theme.kt
+++ app/src/main/java/com/example/android/insightnews/ui/theme/Theme.kt
@@ -10,14 +10,43 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme()
+private val DarkColorScheme = darkColorScheme(
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    secondary = md_theme_dark_secondary,
+    onSecondary = md_theme_dark_onSecondary,
+    background = md_theme_dark_background,
+    onBackground = md_theme_dark_onBackground,
+    surface = md_theme_dark_surface,
+    onSurface = md_theme_dark_onSurface,
+    surfaceVariant = md_theme_dark_onSurfaceVariant,
+    error = md_theme_dark_error,
+    onError = md_theme_dark_onError,
+    outline = md_theme_dark_outline,
+    scrim = md_theme_dark_scrim,
+)
 
-private val LightColorScheme = lightColorScheme()
+private val LightColorScheme = lightColorScheme(
+    primary = md_theme_light_primary,
+    onPrimary = md_theme_light_onPrimary,
+    secondary = md_theme_light_secondary,
+    onSecondary = md_theme_light_onSecondary,
+    background = md_theme_light_background,
+    onBackground = md_theme_light_onBackground,
+    surface = md_theme_light_surface,
+    onSurface = md_theme_light_onSurface,
+    surfaceVariant = md_theme_light_onSurfaceVariant,
+    error = md_theme_light_error,
+    onError = md_theme_light_onError,
+    outline = md_theme_light_outline,
+    scrim = md_theme_light_scrim,
+)
 
 @Composable
 fun InsightNewsTheme(
@@ -38,7 +67,7 @@ fun InsightNewsTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = Color.Transparent.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
