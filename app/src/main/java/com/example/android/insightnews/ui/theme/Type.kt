1b31ad17a4c3eeb7a87d1f9d574fa38738735963
artNehay <artemynehay@gmail.com>
2024-03-05 13:16:21 +0300

    Add Inter font family to the project

--- app/src/main/java/com/example/android/insightnews/ui/theme/Type.kt
+++ app/src/main/java/com/example/android/insightnews/ui/theme/Type.kt
@@ -1,5 +1,28 @@
 package com.example.android.insightnews.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.android.insightnews.R
+
+private val InterFontFamily = FontFamily(
+    Font(
+        resId = R.font.inter_bold,
+        weight = FontWeight.Bold,
+    ),
+    Font(
+        resId = R.font.inter_semibold,
+        weight = FontWeight.SemiBold,
+    ),
+    Font(
+        resId = R.font.inter_medium,
+        weight = FontWeight.Medium,
+    ),
+    Font(
+        resId = R.font.inter,
+        weight = FontWeight.Normal,
+    ),
+)
 
 val Typography = Typography()
