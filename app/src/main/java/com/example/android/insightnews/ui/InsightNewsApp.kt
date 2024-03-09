d158de454fd8a759aa64baba85c7165043ffe07b
artNehay <artemynehay@gmail.com>
2024-03-09 20:32:04 +0300

    Implement a SearchBar button for the Explore screen

--- app/src/main/java/com/example/android/insightnews/ui/InsightNewsApp.kt
+++ app/src/main/java/com/example/android/insightnews/ui/InsightNewsApp.kt
@@ -1,5 +1,6 @@
 package com.example.android.insightnews.ui
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,8 +9,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.android.insightnews.R
 import com.example.android.insightnews.ui.screens.ExploreScreen
+import com.example.android.insightnews.ui.theme.InsightNewsTheme
 
 @Composable
 fun InsightNewsApp() {
@@ -17,8 +20,9 @@ fun InsightNewsApp() {
         modifier = Modifier.fillMaxSize(),
         bottomBar = { BottomNavBar() },
         contentWindowInsets = WindowInsets(
-            left = dimensionResource(R.dimen.screen_margin),
-            right = dimensionResource(R.dimen.screen_margin),
+            left = dimensionResource(R.dimen.horizontal_screen_margin),
+            top = dimensionResource(R.dimen.top_screen_margin),
+            right = dimensionResource(R.dimen.horizontal_screen_margin),
         )
     ) { insetsPadding ->
         ExploreScreen(
@@ -26,5 +30,16 @@ fun InsightNewsApp() {
                 .padding(insetsPadding)
                 .consumeWindowInsets(insetsPadding)
         )
+    }
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL,
+    showBackground = true,
+)
+@Composable
+private fun ExploreScreenPreview() {
+    InsightNewsTheme {
+        InsightNewsApp()
     }
 }
--- app/src/main/java/com/example/android/insightnews/ui/screens/ExploreScreen.kt
+++ app/src/main/java/com/example/android/insightnews/ui/screens/ExploreScreen.kt
@@ -1,11 +1,81 @@
 package com.example.android.insightnews.ui.screens
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.android.insightnews.R
+import com.example.android.insightnews.ui.theme.InsightNewsTheme
 
 @Composable
 fun ExploreScreen(
     modifier: Modifier = Modifier,
 ) {
+    LazyColumn(modifier.fillMaxSize()) {
+        item {
+            Text(
+                text = stringResource(R.string.explore_screen_title),
+                style = MaterialTheme.typography.titleMedium,
+            )
+        }
+        item {
+            SearchBarButton(
+                modifier = Modifier
+                    .padding(vertical = dimensionResource(R.dimen.large_content_padding)),
+            )
+        }
+    }
+}
 
+@Composable
+fun SearchBarButton(
+    modifier: Modifier = Modifier,
+) {
+    Surface(
+        modifier = modifier.fillMaxWidth(),
+        shape = MaterialTheme.shapes.medium,
+        color = MaterialTheme.colorScheme.surfaceVariant,
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(vertical = dimensionResource(R.dimen.medium_content_padding)),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.search_icon),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(horizontal = dimensionResource(R.dimen.medium_content_padding))
+                    .size(dimensionResource(R.dimen.small_icon_size)),
+            )
+
+            Text(
+                text = stringResource(R.string.search_content_description),
+                style = MaterialTheme.typography.bodyLarge,
+            )
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+)
+@Composable
+private fun ExploreScreenPreview() {
+    InsightNewsTheme {
+        ExploreScreen()
+    }
 }
