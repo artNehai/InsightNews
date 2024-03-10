0d653469e8550d0d0dbec70b6b39a1afcd8f01c9
artNehay <artemynehay@gmail.com>
2024-03-10 16:27:56 +0300

    Implement an Editor's Choice card composable

--- app/src/main/java/com/example/android/insightnews/ui/screens/ExploreScreen.kt
+++ app/src/main/java/com/example/android/insightnews/ui/screens/ExploreScreen.kt
@@ -1,11 +1,19 @@
 package com.example.android.insightnews.ui.screens
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,6 +21,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -26,16 +36,26 @@ fun ExploreScreen(
 ) {
     LazyColumn(modifier.fillMaxSize()) {
         item {
-            Text(
-                text = stringResource(R.string.explore_screen_title),
-                style = MaterialTheme.typography.titleMedium,
-            )
-        }
-        item {
-            SearchBarButton(
-                modifier = Modifier
-                    .padding(vertical = dimensionResource(R.dimen.large_content_padding)),
-            )
+            Column {
+                Text(
+                    text = stringResource(R.string.explore_screen_title),
+                    style = MaterialTheme.typography.titleMedium,
+                )
+
+                SearchBarButton(
+                    modifier = Modifier
+                        .padding(vertical = dimensionResource(R.dimen.large_content_padding)),
+                )
+
+                Text(
+                    text = stringResource(R.string.editor_choice_section),
+                    style = MaterialTheme.typography.headlineSmall,
+                )
+
+                Spacer(Modifier.height(dimensionResource(R.dimen.medium_content_spacer)))
+
+                EditorChoiceCard()
+            }
         }
     }
 }
@@ -68,6 +88,78 @@ fun SearchBarButton(
             )
         }
     }
+}
+
+@Composable
+fun EditorChoiceCard(
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        onClick = {},
+        modifier = modifier
+            .width(dimensionResource(R.dimen.editor_choice_card_width))
+            .height(dimensionResource(R.dimen.editor_choice_card_height)),
+    ) {
+        Column(
+            modifier = Modifier.padding(dimensionResource(R.dimen.medium_content_padding)),
+        ) {
+            Image(
+                painter = painterResource(EditorCardPlaceholder.image),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillWidth,
+            )
+
+            Spacer(Modifier.height(dimensionResource(R.dimen.medium_content_spacer)))
+
+            Text(
+                text = EditorCardPlaceholder.title,
+                maxLines = 2,
+                style = MaterialTheme.typography.headlineSmall,
+            )
+
+            Row(
+                modifier = Modifier
+                    .padding(vertical = dimensionResource(R.dimen.medium_content_padding)),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Image(
+                    painter = painterResource(EditorCardPlaceholder.resourceImage),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(dimensionResource(R.dimen.small_icon_size))
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop,
+                )
+
+                Spacer(Modifier.width(dimensionResource(R.dimen.medium_content_spacer)))
+
+                Text(
+                    text = EditorCardPlaceholder.resourceName,
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    style = MaterialTheme.typography.labelMedium,
+                )
+            }
+
+            Text(
+                text = "${EditorCardPlaceholder.addedTime} | ${EditorCardPlaceholder.timeToRead}",
+                color = MaterialTheme.colorScheme.onTertiaryContainer,
+                style = MaterialTheme.typography.labelMedium,
+            )
+        }
+    }
+}
+
+object EditorCardPlaceholder {
+    @DrawableRes
+    val image = R.drawable.article_image_placeholder
+    const val title = "Is Blender the Future of 3D modeling and VFX?"
+
+    @DrawableRes
+    val resourceImage = R.drawable.article_image_placeholder
+    val resourceName = "UX Collective"
+    val addedTime = "20 min ago"
+    val timeToRead = "6 min read"
 }
 
 @Preview(
