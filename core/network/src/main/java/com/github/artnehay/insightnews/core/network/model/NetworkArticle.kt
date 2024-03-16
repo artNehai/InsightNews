da99a482caf0434a8e50e9d3bd85109109c4deed
artNehay <artemynehay@gmail.com>
2024-03-16 14:38:13 +0300

    Add default values to serializable data classes

--- core/network/src/main/java/com/github/artnehay/insightnews/core/network/model/NetworkArticle.kt
+++ core/network/src/main/java/com/github/artnehay/insightnews/core/network/model/NetworkArticle.kt
@@ -5,17 +5,17 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class NetworkArticle(
     val source: NetworkSourceHeader,
-    val author: String,
-    val title: String,
-    val description: String,
-    val url: String,
-    val urlToImage: String,
-    val publishedAt: String,
-    val content: String,
+    val author: String = "",
+    val title: String = "",
+    val description: String = "",
+    val url: String = "",
+    val urlToImage: String = "",
+    val publishedAt: String = "",
+    val content: String = "",
 )
 
 @Serializable
 data class NetworkSourceHeader(
-    val id: String,
-    val name: String,
+    val id: String = "",
+    val name: String = "",
 )
