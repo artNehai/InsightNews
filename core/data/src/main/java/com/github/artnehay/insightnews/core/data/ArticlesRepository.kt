73ccc20f60d41912591fa6ccc34cdee69b3df08e
artNehay <artemynehay@gmail.com>
2024-04-15 17:21:12 +0300

    Inject a news database through LocalDataModule

--- core/data/src/main/java/com/github/artnehay/insightnews/core/data/ArticlesRepository.kt
+++ core/data/src/main/java/com/github/artnehay/insightnews/core/data/ArticlesRepository.kt
@@ -2,6 +2,7 @@ package com.github.artnehay.insightnews.core.data
 
 import com.github.artnehay.insightnews.core.data.di.NewsApiDataSource
 import com.github.artnehay.insightnews.core.data.util.toArticle
+import com.github.artnehay.insightnews.core.database.NewsDatabase
 import com.github.artnehay.insightnews.core.model.Article
 import com.github.artnehay.insightnews.core.network.NewsRemoteDataSource
 import com.github.artnehay.insightnews.core.network.model.NetworkArticle
@@ -12,6 +13,7 @@ import javax.inject.Singleton
 class ArticlesRepository @Inject constructor(
     @NewsApiDataSource
     private val newsApiRemoteDataSource: NewsRemoteDataSource,
+    private val newsDatabase: NewsDatabase,
 ) {
     suspend fun getTopHeadlines(): List<Article> =
         newsApiRemoteDataSource
--- core/data/src/main/java/com/github/artnehay/insightnews/core/data/di/RemoteDataModule.kt
+++ core/data/src/main/java/com/github/artnehay/insightnews/core/data/di/RemoteDataModule.kt
@@ -1,25 +1,37 @@
 package com.github.artnehay.insightnews.core.data.di
 
+import android.content.Context
+import com.github.artnehay.insightnews.core.database.NewsDatabase
 import com.github.artnehay.insightnews.core.network.NewsApiRemoteDataSource
 import com.github.artnehay.insightnews.core.network.NewsRemoteDataSource
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Qualifier
-import javax.inject.Singleton
 
 @Qualifier
 annotation class NewsApiDataSource
 
 @InstallIn(SingletonComponent::class)
 @Module
-abstract class DataModule {
+abstract class RemoteDataModule {
 
     @NewsApiDataSource
-    @Singleton
     @Binds
     abstract fun bindNewsRemoteDataSource(
         newsApiRemoteDataSource: NewsApiRemoteDataSource,
     ): NewsRemoteDataSource
+}
+
+@InstallIn(SingletonComponent::class)
+@Module
+object LocalDataModule {
+
+    @Provides
+    fun provideNewsDatabase(
+        @ApplicationContext context: Context,
+    ) = NewsDatabase.getDatabase(context)
 }
