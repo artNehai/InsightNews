3d17172281e833b0ca4a9af9fa1ef89168b7f710
artNehay <artemynehay@gmail.com>
2024-05-04 21:04:47 +0300

    Implement a scrollable tab row of categories instead of a lazy column

--- feature/explore/src/main/java/com/github/artnehay/insightnews/feature/explore/ExploreScreen.kt
+++ feature/explore/src/main/java/com/github/artnehay/insightnews/feature/explore/ExploreScreen.kt
@@ -18,28 +18,30 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SecondaryScrollableTabRow
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -89,6 +91,7 @@ fun ExploreScreen(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ResultScreen(
     viewModel: ExploreViewModel,
@@ -146,23 +149,23 @@ fun ResultScreen(
             Column {
                 Spacer(Modifier.height(dimensionResource(large_content_spacer)))
 
-                LazyRow(
-                    horizontalArrangement = Arrangement.spacedBy(
-                        dimensionResource(medium_content_spacer)
-                    )
+                val chosenCategory by screenUiState.category
+                SecondaryScrollableTabRow(
+                    selectedTabIndex = Category.entries.indexOf(chosenCategory),
+                    edgePadding = 0.dp,
                 ) {
-                    val chosenCategory by screenUiState.category
-                    items(Category.entries) { category ->
-                        val isChosen = category == chosenCategory
-                        Text(
-                            text = "$category",
-                            color = if (isChosen) {
-                                MaterialTheme.colorScheme.primary
-                            } else {
-                                Color.Unspecified
+                    Category.entries.forEach {
+                        Tab(
+                            selected = it == chosenCategory,
+                            onClick = { /*TODO*/ },
+                            text = {
+                                Text(
+                                    text = it.name,
+                                    style = MaterialTheme.typography.headlineSmall,
+                                )
                             },
-                            textDecoration = if (isChosen) TextDecoration.Underline else null,
-                            style = MaterialTheme.typography.headlineSmall,
+                            selectedContentColor = MaterialTheme.colorScheme.primary,
+                            unselectedContentColor = MaterialTheme.colorScheme.onBackground,
                         )
                     }
                 }
@@ -180,7 +183,7 @@ fun ResultScreen(
                 modifier = Modifier.padding(vertical = dimensionResource(medium_content_spacer)),
                 timeCaption = timeCaption,
             )
-            if (article != screenUiState.topHeadlines.last()) {
+            if (article != categorisedHeadlines.last()) {
                 HorizontalDivider()
             }
         }
